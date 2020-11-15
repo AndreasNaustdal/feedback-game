fb3634ac9103fc1f28dcd61395e09ed9a0af5c73
Andreas Naustdal <andreas.naustdal@netlife.com>
2020-11-15 22:55:32 +0100

    set port from env

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(process.env.PORT || 3000, () => {
+  console.log("listening on *:" + process.env.PORT || 3000);
 });
 
 io.on("connection", socket => {
